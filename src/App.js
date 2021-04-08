bec912ff3512b8d3cc07115739ac4f52359e3a71
giancode1 <gieg5c@gmail.com>
2021-04-07 19:24:02 -0500

    React1 y gh-pages

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre = "Gian";
+  let numeros = [1,2,3];
+  let auth = true;
   return (
-    <div className="App">
+    /* TODO ESTO ES JSX no HTML */
+    <>
+      <div className="App">
       <header className="App-header">
+      <h3>Hola {nombre}</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gian <code>Code</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://giancode.com" target="_blank"
+          rel="noopener noreferrer">Perfil
         </a>
+        suma {2+2}
+        <p>El usuario {auth?"esta logeado":"no esta logeado"}</p>
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre"></input>
+        <ul>
+          {numeros.map((x,index)=>(
+            <li key={index}>{x}</li>
+          ))}
+        </ul>
       </header>
     </div>
+    </>
   );
 }
 
